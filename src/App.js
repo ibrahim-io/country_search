18f5f36d5238e25fc122119cd7cbd4106a218c6a
Ibrahim Ibrahim <ii120@ic.ac.uk>
2022-03-28 18:31:29 +0100

    feat: Set up app to search for countries and provide relevant information

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
+const Display = ({countries, search}) => {
+  if(countries.length === 1) {
+    const country = countries[0]
+    return (
+      <>
+        <h1>{country.name.common}</h1>
+        <h5>Capital: {country.capital[0]}</h5>
+      </>
+    )
+  } else if (countries.length <= 10) {
+    return (
+      <ul>
+        {countries.map(country => {
+          return (
+            <li key={country.name.common}>{country.name.common}</li>
+          )
+        })}
+      </ul>
+    )
+  } else if (search !== '') {
+    return (
+      <p>Too many countries found, please be more specific</p>
+    )
+  } else {
+    return (
+      <p>Please search for a country to get its information</p>
+    )
+  }
+}
+
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [findCountry, setFindCountry] = useState('')
+
+  useEffect(() => axios.get('https://restcountries.com/v3.1/all').then(response => {
+    setCountries(response.data)
+  }), [])
+
+  const changeFindCountry = (event) => {
+    setCountries(countries.filter(country => {
+      return country.name.common.toLowerCase().includes(findCountry)
+    }))
+    setFindCountry(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Country Search</h1>
+      <form>
+        Find Country: <input value={findCountry} onChange={changeFindCountry} />
+      </form>
+      <Display countries={countries} search={findCountry}/>
+    </>
   );
 }
 
