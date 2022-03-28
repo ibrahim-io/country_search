331175ad686e7e80c50fd4de1d0618ee5b354530
Ibrahim Ibrahim <ii120@ic.ac.uk>
2022-03-28 19:00:35 +0100

    feat:  view a country at the click a of a button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const Display = ({countries, search}) => {
+const Display = ({countries, search, showCountry}) => {
+  let id = 1
   if(countries.length === 1) {
     const country = countries[0]
     return (
       <>
         <h1>{country.name.common}</h1>
         <h5>Capital: {country.capital[0]}</h5>
+        <h5>Area: {country.area}</h5>
+        <img src={country.flags.png} alt="country flag"/>
       </>
     )
   } else if (countries.length <= 10) {
     return (
       <ul>
         {countries.map(country => {
+          console.log(id)
           return (
-            <li key={country.name.common}>{country.name.common}</li>
+            <div key={id++}>
+              <li >{country.name.common}</li> <button onClick={() => showCountry([country])}>show</button>
+            </div>
           )
         })}
       </ul>
@@ -41,10 +47,10 @@ const App = () => {
   }), [])
 
   const changeFindCountry = (event) => {
+    setFindCountry(event.target.value)
     setCountries(countries.filter(country => {
       return country.name.common.toLowerCase().includes(findCountry)
     }))
-    setFindCountry(event.target.value)
   }
 
   return (
@@ -53,7 +59,7 @@ const App = () => {
       <form>
         Find Country: <input value={findCountry} onChange={changeFindCountry} />
       </form>
-      <Display countries={countries} search={findCountry}/>
+      <Display countries={countries} search={findCountry} showCountry={setCountries}/>
     </>
   );
 }
